02e161f1f5dc967c21200e9c1b5ec926d1c8fbfa
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-06-03 20:09:33 -0300

    Adicionado botão de seleção na lista principal

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,13 @@
-INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Praia do Caribe', 'Praias', 'https://example.com/caribe.jpg', 8500.00);
+INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Praia do Caribe', 'Praias', 'https://magazine.zarpo.com.br/wp-content/uploads/2022/02/capa_praia-do-caribe_zarpo-770x515.jpg', 8500.00);
 
-INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Fernando de Noronha', 'Praias', 'https://example.com/noronha.jpg', 6500.00);
+INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Fernando de Noronha', 'Praias', 'https://www.melhoresdestinos.com.br/wp-content/uploads/2022/02/fernando-noronha-capa-2022-1536x805.jpg', 6500.00);
 
 INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Orlando', 'Parques', 'https://example.com/orlando.jpg', 9200.00);
 
 INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Nova York', 'Cidades', 'https://example.com/ny.jpg', 9900.00);
+
+INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Rio de Janeiro', 'Cidades', 'https://example.com/rj.jpg', 1900.00);
+
+INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Fortaleza', 'Cidades', 'https://example.com/Fortaleza.jpg', 5980.00);
+
+INSERT INTO destino (local, categoria, imagem_url, preco) VALUES ('Ibirapuera', 'Parques', 'https://grupospimovel.s3.amazonaws.com/Imagens/Noticias/Sistema/mercado-imobiliario/Parque_Ibirapuera.jpg', 1980.00);
